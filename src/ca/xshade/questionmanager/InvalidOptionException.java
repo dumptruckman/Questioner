56fef810c1715e2304cd43ba04e8f3561e7927fa
Jeremy Wood <farachan@gmail.com>
2012-04-22 17:22:07 -0400

    Converted project to maven.

--- src/ca/xshade/questionmanager/InvalidOptionException.java
+++
@@ -1,9 +0,0 @@
-package ca.xshade.questionmanager;
-
-public class InvalidOptionException extends Exception {
-	private static final long serialVersionUID = -8231537623701825085L;
-	
-	public InvalidOptionException() {
-		super("That is not a valid option.");
-	}
-}
--- src/ca/xshade/questionmanager/LinkedQuestionTask.java
+++
@@ -1,6 +0,0 @@
-package ca.xshade.questionmanager;
-
-public abstract class LinkedQuestionTask extends QuestionTask {
-	@Override
-	public abstract void run();
-}
